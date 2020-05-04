8e83830f127c2241ceff5d5dc65d6b164423ef3d
Guicortiz <guilhermechenciortiz@gmail.com>
2020-05-04 10:03:58 -0300

    Corrigindo dicas do index

--- src/components/Dicas/index.js
+++ src/components/Dicas/index.js
@@ -11,76 +11,34 @@ class Dicas extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
   return(
-    /*
-    <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
+     <div className="columns is-multiline">
+        <Jumbotron className='dicasMain'>
+          <CardDeck>
+     {posts &&
+      posts.map(({ node: post }) => (
+       
+            <Card className='dicasCard' >
+              <PreviewCompatibleImage
                         imageInfo={{
                           image: post.frontmatter.featuredimage,
                           alt: `featured image thumbnail for post ${post.frontmatter.title}`,
                         }}
                       />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
-          ))}
-      </div>
-      */
-     <div className="columns is-multiline">
-     {posts &&
-      posts.map(({ node: post }) => (
-        <Jumbotron className='dicasMain'>
-          <CardDeck>
-            <Card className='dicasCard' >
-              <Card.Img variant="top" src='https://img.freepik.com/fotos-gratis/gotas-de-oleo-na-imagem-abstrata-padrao-psicodelico-de-agua_23-2148290141.jpg?size=626&ext=jpg' />
               <Card.Body>
                 <Card.Title className='dicasText'>{post.frontmatter.title}</Card.Title>
-                <Card.Text className='dicasText'>
-                {post.excerpt}
-                </Card.Text>
+                <Card.Text className='dicasText'>{post.description}</Card.Text>
                 < Button variant="primary"><Link className="button" to={post.fields.slug}>
                         Leia mais→
                       </Link></Button>
               </Card.Body>
               <Card.Footer>
-                <small className='dicasText'>Last updated 3 mins ago</small>
+                <small className='dicasText'>{post.frontmatter.date}</small>
               </Card.Footer>
             </Card>
-          </CardDeck>
-        </Jumbotron>
+          
       ))}
+      </CardDeck>
+        </Jumbotron>
       </div>
     )
   }
@@ -98,10 +56,9 @@ export default () => (
   <StaticQuery
     query={graphql`
     query Dicas {
-      allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {frontmatter: {templateKey: {eq: "blog-post"}}}) {
+      allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {frontmatter: {templateKey: {eq: "blog-post"}}}, limit: 3) {
         edges {
           node {
-            excerpt(pruneLength: 400)
             id
             fields {
               slug
@@ -113,16 +70,17 @@ export default () => (
               featuredpost
               featuredimage {
                 childImageSharp {
-                  fluid(maxWidth: 120, quality: 100) {
+                  fluid(maxWidth: 800, quality: 100) {
                     ...GatsbyImageSharpFluid
                   }
                 }
               }
+              description
             }
           }
         }
       }
-    }    
+    }      
     `}
     render={(data, count) => <Dicas data={data} count={count} />}
   />
